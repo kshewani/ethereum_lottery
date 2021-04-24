f6072886e352929d4cb9d0c668f500341934d81f
Kamlesh Shewani <kamlesh.shewani@gmail.com>
2021-04-24 19:10:08 +0800

    added test file and compile script

---
+++ compile.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const fs = require('fs');
+const solc = require('solc');
+
+const lotteryPath = path.resolve(__dirname, 'contracts', 'Lottery.sol');
+source = rs.readFileSync(lotteryPath, 'utf8');
+
+var input = {
+    language: 'Solidity',
+    sources: {
+        'Lottery.sol' : {
+            content: source
+        }
+    },
+    settings: {
+        outputSelection: {
+            '*': {
+                '*': [ '*' ]
+            }
+        }
+    }
+}; 
+var output = JSON.parse(solc.compile(JSON.stringify(input)));
+module.exports = output.contracts['Lottery.sol']['Lottery'];
